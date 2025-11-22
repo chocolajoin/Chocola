9e7ca5bfcc7a034a1f337c22597cb7af728f0212
chocolajoin <peruleyend3@gmail.com>
2025-11-21 22:15:21 -0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "roblox-autojoin-api",
+  "version": "1.0.0",
+  "description": "API para auto-join de Roblox",
+  "main": "api/teleport.js",
+  "scripts": {
+    "dev": "vercel dev"
+  },
+  "dependencies": {
+    "axios": "^1.6.0"
+  },
+  "engines": {
+    "node": "22.x"
+  }
+}
